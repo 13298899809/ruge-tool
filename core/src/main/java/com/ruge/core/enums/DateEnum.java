10fe7fe8bc166fd548c412040e838a6fa1fa5515
ruge.wu <845376854>
2020-08-04 14:55:01 +0800

    日期枚举类

---
+++ core/src/main/java/com/ruge/core/enums/DateEnum.java
@@ -0,0 +1,16 @@
+package com.ruge.core.enums;
+
+/**
+ * @author ruge.wu
+ * @version 0.0.1
+ * @ClassName DateEnum
+ * @date 2020.08.04 14:52
+ */
+public enum  DateEnum {
+    YEAR(),
+    MONTH(),
+    DAY(),
+    HOUR(),
+    MINUTE(),
+    SECOND()
+}
